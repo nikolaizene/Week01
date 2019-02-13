a64062d83e9a41ba472e25c45e07007d3ffd8327
Nikolai Zenevitš <nizene@ttu.ee>
2019-02-13 20:50:08 +0200

    Commit

---
+++ Creating and adding references to assemblies vol 2/Scrape.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace Creating_and_adding_references_to_assemblies_vol_2
+{
+    public class Scrape
+    {
+        public string ScrapeWebpage(string url)
+        {
+            return GetWebpage(url);
+        }
+
+        public string ScarpeWebpage(string, url, string filepath)
+        {
+
+            string reply = GetWebpage(url);
+
+            File.WriteAllText(filepath, reply);
+            return reply;
+
+        }
+
+        private string GetWebpage(string url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+        }
+    }
+}
---
+++ Creating and adding references to assemblies/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creating_and_adding_references_to_assemblies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
