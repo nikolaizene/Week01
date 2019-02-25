d7a024d4aa60c805048b5aa61d138cabc13a33a5
Nikolai Zenevitš <nizene@ttu.ee>
2019-02-25 23:51:06 +0200

    Commit

---
+++ Week04Homework/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Week04Homework.Models;
+
+namespace Week04Homework.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "Hello world!";
+        }
+        public ActionResult GetView ()
+        {
+            Employee emp = new Employee();
+            emp.FirstName = "Nikolai";
+            emp.LastName = "Zenevitsh";
+            emp.Salary = 100000;
+
+            ViewBag["Employee"] = emp;
+            return View("MyView");
+        }
+    }
+}
---
+++ Week04Homework/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Week04Homework.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
